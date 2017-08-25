f80e451ebd7bb67687e9dd06e31aad2b4790bd57
git <398631274@qq.com>
2017-08-25 14:13:48 +0800

    test with wstmall of thinkphp

---
+++ Apps/Common/Conf/wst_config.php
@@ -0,0 +1,12 @@
+<?php
+ return array(
+	    'WST_VERSION' => '1.9.4_170630',
+	    'WST_MD5' => 'fd3d8bce9cc9879408a82da5f1598034',
+        'WST_M_IMG_SUFFIX'   => '',
+	    'WST_RUNTIME_PATH'=>dirname(dirname(dirname(__File__)))."/Runtime",
+        'DEFAULT_FILTER'=>'trim,htmlspecialchars',
+        'WST_FEEDBACK'=>'http://www.wstmall.net/index.php?m=Home&c=Articles&a=index&id=8',
+        'WST_WEB'=>'http://www.wstmall.net',
+ 		'WST_UPLOAD_DIR'=>'wechat,appraises,adspic,brands,complains,file,friendlinks,shops,staffs,users,mall,goods,complains,temp,uploads'
+	);
+?>
